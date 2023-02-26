f01582f8bd449b81a2d8d152f87391dd03598255
dydgh142 <dydgh142@naver.com>
2023-02-26 11:10:34 +0900

    [feat] vite.config 새 기능 추가

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+    cors: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
